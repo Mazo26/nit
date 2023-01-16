7e990a68f8eeea4e207a54e3000ebebadb1c4bde
Mazo26 <mazo.creed26@gmail.com>
2023-01-16 18:39:55 +0100

    React beginning

--- tips.js
+++ tips.js
@@ -74,8 +74,10 @@ const arr = [
 
 // cd - change directory je komanda u terminalu
 // koja sluzi za navigiranje kroz foldere unutar sistema
+
 // ls - je komanda koja sluzi za izlistavanje
 // fajlova i foldera unutar foldera
+
 // mkdir - make directory je komanda koja sluzi
 // za kreiranje novog direktorija ili foldera
 
