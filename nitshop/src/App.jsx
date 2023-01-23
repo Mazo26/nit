2bfa314cc0d46e0465b0f48ceb138fb81071e857
Mazo26 <mazo.creed26@gmail.com>
2023-01-23 20:59:42 +0100

    Children & Parent

--- nitshop/src/App.jsx
+++ nitshop/src/App.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import CustomForm from "./components/CustomForm/CustomForm";
+import CustomDiv from "./components/CustomDiv/CustomDiv";
 
 const App = () => {
+  const [fullName, setFullName] = useState("");
+
+  function dataGrabber(name, lastName) {
+    let fullName = name + " " + lastName;
+    console.log(fullName);
+    setFullName(fullName);
+  }
+
   return (
     <div className='App'>
       <header className='App-header'>
-        <CustomForm />
+        <CustomDiv>
+          <h4>{fullName}</h4>
+        </CustomDiv>
+        <CustomForm sendData={(name, lName) => dataGrabber(name, lName)} />
       </header>
     </div>
   );
--- nitshop/src/components/CustomForm/CustomForm.jsx
+++ nitshop/src/components/CustomForm/CustomForm.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import StyledInput from "../StyledInput/StyledInput";
 import StyledButton from "../StyledButton/StyledButton";
 
-const CustomForm = () => {
+const CustomForm = (props) => {
   const [name, setName] = useState("");
   const [lastName, setLastName] = useState("");
 
@@ -18,7 +18,10 @@ const CustomForm = () => {
     <div>
       <StyledInput onChangeHandler={nameHandler} value={name} />
       <StyledInput onChangeHandler={lastNameHandler} value={lastName} />
-      <StyledButton innerText='Send' />
+      <StyledButton
+        onClickHandler={() => props.sendData(name, lastName)}
+        innerText='Send'
+      />
     </div>
   );
 };
