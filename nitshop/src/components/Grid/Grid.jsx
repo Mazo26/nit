2dd0f0f48cc8c84d855ddf739262415cce24da2f
Mazo26 <mazo.creed26@gmail.com>
2023-02-03 20:58:11 +0100

    GridMUI2

--- nitshop/src/components/Grid/Grid.jsx
+++ nitshop/src/components/Grid/Grid.jsx
@@ -4,29 +4,46 @@ import Grid from "@mui/material/Grid";
 
 const GridCmp = () => {
   return (
-    <Grid container spacing={2}>
+    <Grid container>
       <Grid
         container
         direction='row'
         justifyContent='center'
         alignItems='flex-end'
-        spacing={2}
       >
-        <CustomDiv width='30%'> xs=8 </CustomDiv>
-        <CustomDiv width='30%'> xs=8 </CustomDiv>
-        <CustomDiv width='30%'> xs=8 </CustomDiv>
+        <Grid item xs={2}>
+          <CustomDiv width='100%'> xs=2 </CustomDiv>
+        </Grid>
+        <Grid item xs={6}>
+          <CustomDiv width='100%'> xs=6 </CustomDiv>
+        </Grid>
+        <Grid item xs={4}>
+          <CustomDiv width='100%'> xs=4 </CustomDiv>
+        </Grid>
       </Grid>
-      <Grid item xs={6} sm={10} md={8} lg={6}>
+      <Grid item xs={8} sm={12} md={12} lg={12}>
         <CustomDiv width='100%'> xs=8 </CustomDiv>
       </Grid>
-      <Grid item xs={6} sm={2} md={4} lg={6}>
+      <Grid item xs={4} sm={12} md={12} lg={12}>
         <CustomDiv width='100%'> xs=4 </CustomDiv>
       </Grid>
-      <Grid item xs={6}>
-        <CustomDiv width='100%'> xs=6 </CustomDiv>
+      <Grid item xs={2}>
+        <CustomDiv width='100%'> xs=2 </CustomDiv>
       </Grid>
-      <Grid item xs={6}>
-        <CustomDiv width='100%'> xs=6 </CustomDiv>
+      <Grid item xs={2}>
+        <CustomDiv width='100%'> xs=2 </CustomDiv>
+      </Grid>
+      <Grid item xs={2}>
+        <CustomDiv width='100%'> xs=2 </CustomDiv>
+      </Grid>
+      <Grid item xs={2}>
+        <CustomDiv width='100%'> xs=2 </CustomDiv>
+      </Grid>
+      <Grid item xs={2}>
+        <CustomDiv width='100%'> xs=2 </CustomDiv>
+      </Grid>
+      <Grid item xs={2}>
+        <CustomDiv width='100%'> xs=2 </CustomDiv>
       </Grid>
     </Grid>
   );
