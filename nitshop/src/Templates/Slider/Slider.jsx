e01f57e9a535905dfcc4f5d039b319a776f8806e
Mazo26 <mazo.creed26@gmail.com>
2023-02-27 20:55:35 +0100

    Styling Carousel and making responsiveness

--- nitshop/src/Templates/Slider/Slider.jsx
+++ nitshop/src/Templates/Slider/Slider.jsx
@@ -2,24 +2,56 @@ import Carousel from "../../components/Carousel/Carousel";
 import girl1 from "../../assets/images/home/girl1.jpg";
 import girl2 from "../../assets/images/home/girl2.jpg";
 import girl3 from "../../assets/images/home/girl3.jpg";
+import SimplifiedDiv from "../../components/SimplifiedDiv/SimplifiedDiv";
+import { Grid } from "@mui/material";
+import { getScreenWidth } from "../../util/helpers";
+
+const sliders = [
+  {
+    imgSrc: girl1,
+    imgText: "Zaova 1",
+  },
+  {
+    imgSrc: girl2,
+    imgText: "Svekrva 2",
+  },
+  {
+    imgSrc: girl3,
+    imgText: "Snaha 3",
+  },
+];
 
 const Slider = () => {
-  const sliders = [
-    {
-      imgSrc: girl1,
-      imgText: "Image Text 1",
-    },
-    {
-      imgSrc: girl2,
-      imgText: "Image text 2",
-    },
-    {
-      imgSrc: girl3,
-      imgText: "Image text 3",
-    },
-  ];
+  const deviceWidth = getScreenWidth();
 
-  return <Carousel data={sliders} />;
+  const setSliderMargin =
+    deviceWidth === "SM"
+      ? "0px"
+      : deviceWidth === "MD"
+      ? "0px 10px"
+      : "0px 10%";
+
+  const setPaddingTop =
+    deviceWidth === "SM" ? "10px" : deviceWidth === "MD" ? "20px" : "30px";
+
+  const styles = {
+    sliderContainer: {
+      width: "100%",
+      height: "200px",
+      paddingTop: setPaddingTop,
+      margin: setSliderMargin,
+    },
+  };
+
+  return (
+    <Grid container direction='row'>
+      <Grid item xs={12} sm={12} md={12} lg={12}>
+        <SimplifiedDiv style={styles.sliderContainer}>
+          <Carousel data={sliders} />
+        </SimplifiedDiv>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Slider;
--- nitshop/src/components/Carousel/Carousel.jsx
+++ nitshop/src/components/Carousel/Carousel.jsx
@@ -1,13 +1,50 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel as SliderCarousel } from "react-responsive-carousel";
+import Text from "../Text/Text";
+import { colors, fontSize } from "../../util/theme";
+import SimplifiedDiv from "../SimplifiedDiv/SimplifiedDiv";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 
 const Carousel = ({ data }) => {
+  const styles = {
+    carouselText: {
+      position: "absolute",
+      bottom: 30,
+      left: 10,
+      right: 10,
+    },
+  };
+
   return (
-    <SliderCarousel>
+    <SliderCarousel
+      width='100%'
+      axis='horizontal'
+      showIndicators
+      showThumbs={false}
+      interval={1000}
+      autoPlay
+      infiniteLoop
+      showStatus={false}
+      showArrows={false}
+      swipeable
+    >
       {data.map((d) => (
-        <div>
-          <img src={d.imgSrc} />
-          <p className='legend'>{d.imgText}</p>
+        <div key={d} style={{ height: "400px" }}>
+          <img
+            src={d.imgSrc}
+            style={{ height: "100%", objectFit: "contain" }}
+            alt={d.imgText}
+          />
+          <SimplifiedDiv style={styles.carouselText}>
+            <Text
+              fontSize={fontSize.xLarge}
+              color={colors.gray}
+              textAlign='left'
+            >
+              {d.imgText}
+            </Text>
+          </SimplifiedDiv>
         </div>
       ))}
     </SliderCarousel>
